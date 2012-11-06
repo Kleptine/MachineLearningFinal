0155f5fc3dcca4a801cc2b37efc19135afe79962
Jisha <jk2228@cornell.edu>
2012-11-06 17:21:51 -0500

    getVotes() fixed, haven't run on all reps (time)
    added offset to votes, havent run on all files yet - have to run it for
    a long time to generate all files. getReps() throws an exception, added
    try catch- have to debug

--- extract_features.py
+++ extract_features.py
@@ -1,9 +1,11 @@
 #
 #   Extract features from a given bill object
-
 import httplib, json
+
+import sys
 from pprint import pprint
-dict={}
+dictj={}
+reps=[]
 
 
 def extractFeatures(bill):
@@ -16,7 +18,8 @@ def getInputVectors(person, billMap):
     return []
 
 
-#get all representatives, high limit gets all - verified (jisha)
+#get all representatives, high limit gets all - verified (jisha) 
+#not used currently 
 def getPeople():
     connpeople = httplib.HTTPConnection('www.govtrack.us')
     connpeople.request('GET', '/api/v1/person/?limit=1000')
@@ -32,13 +35,20 @@ def getReps():
     votes = []
     conn = httplib.HTTPConnection('www.govtrack.us')
     for i in range(0,7):
-        print i
+        #print i
         conn.request('GET', '/api/v1/person/?limit=2000&offset=' + str(i*2000))
         r2 = conn.getresponse()
-        print r2.reason
-        result = list(json.loads(r2.read())["objects"])
-        if result != None:
-            votes = votes + result
+        #print r2.reason
+        text= r2.read()
+        try:
+            repstext= json.loads(text)
+            result = list(repstext["objects"])
+            if result != None:
+                votes = votes + result
+        except:
+            print "Unexpected error in getReps()", sys.exc_info()[0]         
+            f= open("testReps.txt","w")
+            f.write(text)
     reps = []
     count = 0
     for i in votes:
@@ -48,6 +58,13 @@ def getReps():
                 count = count + 1
                 reps.append(i['id'])
     return reps
+
+def getAllVotes():
+    global reps
+    reps= getReps()
+    print reps
+    for rep in reps:
+        getVotes(rep)
 
 #Getting bills also fails when limit is above 6000 (5000 works) which makes me think this may not be an issue with
 # large size of data coz one api call(votes) fails at 12000 and the other(bills) at 6000. (jisha)
@@ -61,16 +78,32 @@ def getReps():
 #setting a high limit should get all acc to documentation, but fails at limit=12000 (11000 works)
 #puts the list of votes by this person in a file (temp)
 def getVotes (person= None):
-    global dict
+    allvotes=[]
+    global dictj
     conn = httplib.HTTPConnection('www.govtrack.us')
-    conn.request('GET', '/api/v1/vote_voter/?person='+person+'&congress=112&limit=11000&order_by=-created')
-    r1 = conn.getresponse()
-    
-    print r1.reason
-    #bills = json.loads(r1.read())
-    bills = json.loads(r1.read())["objects"]
-    for bill in bills:
-        dict[person]=bill["link"] #TODO store tuple of (id of bill extracted from link , option)
+    for i in range(1,10):
+        conn.request('GET', '/api/v1/vote_voter/?person='+str(person)+'&offset='+str(i*1000)+'&congress=112&limit=1000&order_by=-created')
+        r1 = conn.getresponse()
+        print r1.reason
+        text= r1.read()
+        #bills = json.loads(r1.read()) 
+        #ft.write(text)
+        #ft.close()
+        try:
+            allbills = json.loads(text)
+            bills= allbills["objects"]
+            for bill in bills:
+                print bill
+                allvotes.append(bill)
+                dictj[person]=bill["link"] #TODO store tuple of (id of bill extracted from link , option)
+        except:
+             print "Unexpected error in getVotes", sys.exc_info()[0]
+             ft= open("test.txt","w")
+             print "Person is= "+ str(person)
+             ft= open("test.txt","w")
+             ft.write(text)
     f= open("votes"+person+".txt",'w')
-    f.write(json.dumps(bills))
+    f.write(json.dumps(allvotes))
+        
     #pprint (json.dumps(bills))
+getAllVotes()
